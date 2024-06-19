b25d4e266d20e93403dfb351e470bb88438a4d8c
michellekenia <keniamsoaress@gmail.com>
2024-06-19 19:46:51 -0300

    add lista inicial

--- src/app/colaborador/colaborador.component.ts
+++ src/app/colaborador/colaborador.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+interface Colaboradores {
+  id: number, 
+  nome: string,
+  cargo: string
+ } 
+
 @Component({
   selector: 'app-colaborador',
   templateUrl: './colaborador.component.html',
   styleUrl: './colaborador.component.css'
 })
+
+
 export class ColaboradorComponent {
+
+colaboradores: Colaboradores [] = [
+{ id: 1, nome:'Mariana', cargo: 'Programadora back-end' }, 
+{ id: 2, nome:'Odara', cargo: 'Programadora front-end' },
+{ id: 3, nome:'Kênia', cargo: 'Tech Lead' }
+
+]
+
+trackByItem(index: number, item: Colaboradores): number {
+  return item.id;
+}
+
 
 }
