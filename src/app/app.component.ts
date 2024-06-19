c9557bb5fe73420bf92405bc3333b1b410fdd4f8
michellekenia <keniamsoaress@gmail.com>
2024-06-19 18:34:38 -0300

    ajuste de rota

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+constructor(
+  private router: Router
+){}
+
+
   title = 'Meu gerenciador de colaboradores!';
+
+// ngOnInit(){
+
+// }
+
+
+
+
+
+
+// irParaColaborador(){
+//   this.router.navigate(['/colaborador'])
+// }
+  
 }
