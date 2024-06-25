8fc21bfb72f57b3c5928a272ec48a9fd684f1692
michellekenia <keniamsoaress@gmail.com>
2024-06-25 15:08:43 -0300

    ajustar metodos

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,6 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
+const headers = new HttpHeaders({
+  'Content-type': 'application/json; charset-UTF-8', 'Access-Control-Allow-Origin':  '*'
+})
+const options = {
+  headers
+}
 
 @Injectable({
   providedIn: 'root'
@@ -11,19 +18,19 @@ constructor(private httpClient: HttpClient) { }
 
 
 get<T>(url: string): Observable<T> {
-  return this.httpClient.get<T>(url);
+  return this.httpClient.get<T>(url, options);
 }
 
 post<T>(url: string, body: any): Observable<T>{
-  return this.httpClient.post<T>(url, body)
+  return this.httpClient.post<T>(url, body, options)
 }
 
 put<T>(url: string, body: any): Observable<T>{
-  return this.httpClient.put<T>(url, body)
+  return this.httpClient.put<T>(url, body, options)
 }
 
 delete<T>(url: string): Observable<T>{
-  return this.httpClient.delete<T>(url)
+  return this.httpClient.delete<T>(url, options)
 }
 
 }
