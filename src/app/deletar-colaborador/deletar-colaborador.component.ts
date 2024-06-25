9b64ac788ff0d59c6e774195d5ebae637017fe92
michellekenia <keniamsoaress@gmail.com>
2024-06-25 15:12:17 -0300

    criar metodo delete

--- src/app/deletar-colaborador/deletar-colaborador.component.ts
+++ src/app/deletar-colaborador/deletar-colaborador.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Colaborador } from '../colaborador/colaborador';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-deletar-colaborador',
@@ -8,19 +9,52 @@ import { Colaborador } from '../colaborador/colaborador';
   styleUrl: './deletar-colaborador.component.css'
 })
 export class DeletarColaboradorComponent {
-  constructor(private router: Router) { }
 
-  colaborador: Colaborador = {nome: '', cargo: ''}
+  constructor(
+    private router: Router, 
+    private route: ActivatedRoute,
+    private dataService: DataService
+  ) { }
 
-  colaboradores: Colaborador [] = []
+  
+  colaborador: Colaborador = { id: 0, nome: '', cargo: '' }
+  colaboradores: Colaborador [] = [
 
-  deColaborador(){
-    this.router.navigate(['deletar-colaborador'])
+  ]
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const id = +params['id'];
+      if (id) {
+        this.getColaborador(id);
+      }
+    });
   }
 
-  listarColaborador() {
-    this.router.navigate(['listar-colaborador'])
+  getColaborador(id: number): void {
+    const url = `https://jsonplaceholder.typicode.com/posts/${id}`
+    this.dataService.get<Colaborador>(url)
+      .subscribe((data: any) => {
+        this.colaborador = {
+          nome: data.title,
+          cargo: 'vazio',
+          id: data.id
+        };
+        console.log("Resultado da consulta:", data);
+      });
   }
 
+  deletarColaborador(){
+    const url = `https://jsonplaceholder.typicode.com/posts/${this.colaborador.id}`
+    this.dataService.delete<any>(url)
+    .subscribe(() => {
+      console.log('colaborador deletado com sucesso!')
+      this.router.navigate(['listar-colaborador'])
+    })
+  }
+
+  cancelarDelecao(): void {
+    this.router.navigate(['listar-colaborador']);
+  }
   
 }
