15f3e606bf10c26f83e16c0429fded21d188934a
michellekenia <keniamsoaress@gmail.com>
2024-06-26 16:17:47 -0300

    refatorar metodo deletar

--- src/app/deletar-colaborador/deletar-colaborador.component.ts
+++ src/app/deletar-colaborador/deletar-colaborador.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Colaborador } from '../colaborador/colaborador';
 import { DataService } from '../data.service';
+import { Colaborador } from '../colaborador/colaborador';
 
 @Component({
   selector: 'app-deletar-colaborador',
@@ -9,52 +9,47 @@ import { DataService } from '../data.service';
   styleUrl: './deletar-colaborador.component.css'
 })
 export class DeletarColaboradorComponent {
-
+  isUpdate: boolean = false
+  
   constructor(
     private router: Router, 
     private route: ActivatedRoute,
     private dataService: DataService
   ) { }
 
-  
   colaborador: Colaborador = { id: 0, nome: '', cargo: '' }
-  colaboradores: Colaborador [] = [
 
-  ]
+  ngOnInit(){
 
-  ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      const id = +params['id'];
-      if (id) {
-        this.getColaborador(id);
+    this.route.params.subscribe(params => {     
+      const id = params['id'] 
+      if(id){
+        this.isUpdate = true
+        this.getColaborador(id)
       }
-    });
+    })   
   }
 
-  getColaborador(id: number): void {
-    const url = 'http://localhost:3000/colaboradores/' + id
-    this.dataService.get<Colaborador>(url)
-      .subscribe((data: any) => {
-        this.colaborador = {
-          nome: data.nome,
-          cargo: data.cargo,
-          id: data.id
-        };
-        console.log("Resultado da consulta:", data);
-      });
-  }
+getColaborador(id: number): void{
+  const url = 'http://localhost:3000/colaboradores/'+ id
+  this.dataService.get<Colaborador>(url)
+  .subscribe((data: any)=> {
+    this.colaborador = {
+      id: data.id,
+      nome: data.nome, 
+      cargo: data.cargo,
+      
+    } 
+    console.log("Resultado Get", data)
+  })    
+}
 
-  deletarColaborador(){
-    const url = 'http://localhost:3000/colaboradores' + this.colaborador.id
-    this.dataService.delete<any>(url)
-    .subscribe(() => {
-      console.log('colaborador deletado com sucesso!')
-      this.router.navigate(['listar-colaborador'])
-    })
-  }
-
-  cancelarDelecao(): void {  
-    this.router.navigate(['listar-colaborador']);
-  }
+deletarColaborador(): void {
+  const url = `http://localhost:3000/colaboradores/${this.colaborador.id}`
+  this.dataService.delete<any>(url).subscribe(response => {
+    console.log("foi deletado:", response)
+    this.router.navigate(['listar-colaborador'])
+  })   
+  } 
   
 }
